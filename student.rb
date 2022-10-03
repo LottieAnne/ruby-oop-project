2821e0193771d8c18d4981951cac0ec5f1e6aef5
cchaae <kkang_chung125@yahoo.co.jp>
2022-10-03 13:56:21 +0900

    feat: accessorで一つずつ定義しなくても各項目表示できる

--- student.rb
+++ student.rb
@@ -1,17 +1,19 @@
 class Student
+  attr_accessor :first_name, :last_name, :email, :username
+
   @first_name
   @last_name
   @email
   @username
   @password
   
-  def first_name=(name)
-    @first_name = name
-  end
+  # def first_name=(name)
+  #   @first_name = name
+  # end
 
-  def first_name
-    @first_name
-  end
+  # def first_name
+  #   @first_name
+  # end
 
   def to_s
     "First name: #{@first_name}"
@@ -21,4 +23,12 @@ end
 mashrur = Student.new
 puts mashrur
 mashrur.first_name = "Mashrur"
+mashrur.last_name = "Hossain"
+mashrur.email = "mashrur.example.com"
+mashrur.username = "mashrur1"
+
 puts mashrur.first_name
+puts mashrur.last_name
+puts mashrur.email
+puts mashrur.username
+
