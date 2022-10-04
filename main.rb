01db136c6352a286fc757552e5ccd31838a3c4fb
cchaae <kkang_chung125@yahoo.co.jp>
2022-10-04 11:30:38 +0900

    feat: passwordをハッシュ値で表示

--- main.rb
+++ main.rb
@@ -8,15 +8,17 @@ end
 require 'bcrypt'
 
 my_password = BCrypt::Password.create("my password")
-#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+my_password_1 = BCrypt::Password.create("my password")
+my_password_2 = BCrypt::Password.create("my password")
 
 puts my_password
-puts my_password.version              #=> "2a"
-puts my_password.cost                 #=> 12
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
+puts my_password_1
+puts my_password_2
 
-my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
-my_password == "my password"     #=> true
-my_password == "not my password" #=> false
+puts my_password == "my password"
+
+
+# my_password = BCrypt::Password.new("$2a$12$abO.kdHpZBjppeYydh9TJegvlRC6Pd8mZcCyaSs2JltzYKy4iW0O6")
+# puts my_password == "my password"     #=> true
+# my_password == "not my password" #=> false
 
